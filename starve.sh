5ce5451d2d440c5d29535c139799cec3c1cc42b7
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-03-06 00:36:55 +0400

    pre v0.0.2

--- starve.sh
+++ starve.sh
@@ -7,6 +7,33 @@ echo "Files: "$(find node_modules/ -type f | wc -l)
 
 # Common unneeded files
 find . -type d -name node_modules -prune -exec find {} -type f \( \
+    -iname .DS_Store -or \
+    -iname AUTHORS -or \
+    -iname CHANGELOG -or \
+    -iname CHANGELOG.md -or \
+    -iname CONTRIBUTORS -or \
+    -iname Gruntfile.js -or \
+    -iname Gulpfile.js -or \
+    -iname LICENCE -or \
+    -iname LICENCE-BSD -or \
+    -iname LICENCE-jsbn -or \
+    -iname LICENCE-MIT -or \
+    -iname LICENCE-MIT.txt -or \
+    -iname LICENCE.BSD -or \
+    -iname LICENCE.md -or \
+    -iname LICENCE.txt -or \
+    -iname LICENSE -or \
+    -iname LICENSE-BSD -or \
+    -iname LICENSE-jsbn -or \
+    -iname LICENSE-MIT -or \
+    -iname LICENSE-MIT.txt -or \
+    -iname LICENSE.BSD -or \
+    -iname LICENSE.md -or \
+    -iname LICENSE.txt -or \
+    -iname Makefile -or \
+    -iname NOTICE.txt -or \
+    -iname README -or \
+    -iname README.md -or \
     \( -name '*.ts' -and \! -name '*.d.ts' \) \
   \) -print0 \; | xargs -0 rm -rf
 
