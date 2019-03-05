8273077e9d45f578a65ebb02aa19bae794576b9d
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-03-06 01:00:28 +0400

    More common unnecessary files

--- starve.sh
+++ starve.sh
@@ -3,14 +3,15 @@
 echo "Putting node_modules on diet..."
 echo ""
 echo "Before starvation: "$(du -hs .)
-echo "Files: "$(find node_modules/ -type f | wc -l)
+echo "Files count: "$(find node_modules/ -type f | wc -l)
 
-# Common unneeded files
+# Unnecessary files
 find . -type d -name node_modules -prune -exec find {} -type f \( \
     -iname .DS_Store -or \
     -iname AUTHORS -or \
     -iname CHANGELOG -or \
     -iname CHANGELOG.md -or \
+    -iname CODEOWNERS -or \
     -iname CONTRIBUTORS -or \
     -iname Gruntfile.js -or \
     -iname Gulpfile.js -or \
@@ -35,15 +36,57 @@ find . -type d -name node_modules -prune -exec find {} -type f \( \
     -iname LICENSE.mkd -or \
     -iname LICENSE.txt -or \
     -iname Makefile -or \
+    -iname MIT-LICENSE.txt -or \
     -iname NOTICE.txt -or \
     -iname README -or \
     -iname README.markdown -or \
     -iname README.md -or \
+    -iname README.md~ -or \
     -iname README.mkd -or \
+    -iname UNLICENSE -or \
+    -name _config.yml -or \
+    -name _test -or \
+    -name .appveyor.yml -or \
+    -name .babelrc -or \
+    -name .coveralls.yml -or \
+    -name .documentup.json -or \
+    -name .editorconfig -or \
+    -name .eslintignore -or \
+    -name .eslintrc -or \
+    -name .eslintrc.js -or \
+    -name .eslintrc.json -or \
+    -name .eslintrc.yml -or \
+    -name .flowconfig -or \
+    -name .gitattributes -or \
+    -name .gitlab-ci.yml -or \
+    -name .gitmodules -or \
+    -name .htmllintrc -or \
+    -name .jscs.json -or \
+    -name .jshintrc -or \
+    -name .lint -or \
+    -name .npmignore -or \
+    -name .npmrc -or \
+    -name .prettierrc -or \
+    -name .prettierrc.js -or \
+    -name .prettierrc.json -or \
+    -name .prettierrc.toml -or \
+    -name .prettierrc.yml -or \
+    -name .stylelintrc -or \
+    -name .stylelintrc.js -or \
+    -name .stylelintrc.json -or \
+    -name .stylelintrc.yaml -or \
+    -name .stylelintrc.yml -or \
+    -name .tern-port -or \
+    -name .tern-project -or \
+    -name .travis.yml -or \
+    -name .yarn-integrity -or \
+    -name .yarn-metadata.json -or \
+    -name .yarnclean -or \
+    -name .yo-rc.json -or \
     \( -name '*.ts' -and \! -name '*.d.ts' \) \
   \) -print0 \; | xargs -0 rm -rf
 
-# Common unneeded directories
+# Unnecessary folders
 find . -type d -name node_modules -prune -exec find {} -type d \( \
     -name __tests__ -or \
     -name .circleci -or \
@@ -69,4 +112,4 @@ find . -type d -name node_modules -prune -exec find {} -type d \( \
 
 echo ""
 echo "After starvation: "$(du -hs .)
-echo "Files: "$(find node_modules/ -type f | wc -l)
+echo "Files count: "$(find node_modules/ -type f | wc -l)
